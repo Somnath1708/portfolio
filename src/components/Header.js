53a00750fa659dcf55705226a0a29c92a80a266e
Somnath1708 <somnathpalaniswamy@gmail.com>
2021-06-26 10:47:21 +0530

    small change

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,9 +9,6 @@ export default class Header extends Component {
 						<a className="mobile-btn" href="#nav-wrap" title="Show navigation">
 							Show navigation
 						</a>
-						<a className="mobile-btn" href="#" title="Hide navigation">
-							Hide navigation
-						</a>
 						<ul id="nav" className="nav">
 							<li className="current">
 								<a className="smoothscroll" href="#home">
